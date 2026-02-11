47c3c501f99b686fb4042a7af1d6c37eafd31b64
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2026-02-11 15:59:51 +0100

    Add files via upload

---
+++ 054_ArrayDimTwo.c
@@ -0,0 +1,38 @@
+// Program to print transpose of matrix
+#include <stdio.h>
+
+int main()
+{
+	int a[2][3], i, j;
+	printf("Enter elements of matrix\n");
+	
+	for(i = 0; i < 2; i++)
+	{
+		for(j = 0; j < 3; j++)
+		{
+			scanf("%d", &a[i][j]);
+		}
+	}
+	printf("\n");
+	
+	printf("Matrix is:\n");
+	for(i = 0; i < 2; i++)
+	{
+		for(j = 0; j < 3; j++)
+		{
+			printf("%d\t", a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("\nTranspose of matrix is:\n");
+	for(i = 0; i < 3; i++)
+	{
+		for(j = 0; j < 2; j++)
+		{
+			printf("%d\t", a[j][i]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
